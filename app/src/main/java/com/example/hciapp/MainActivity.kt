6fdbcff0de8496b9ddfdecd1e5f29a40dfdd1fb6
cachoJDG <jgago@itba.edu.ar>
2024-11-14 11:05:56 -0300

    First commit

---
+++ app/src/main/java/com/example/hciapp/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.hciapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.hciapp.ui.theme.HciAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge() // hacer q ocupe el 100% de la pantalla nuestra app
+        setContent {
+            HciAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ShowTwoLabels(){
+    Row {
+        Text(text = "h")
+        Text(text = "W")
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowTwoLabelsPreview() {
+    HciAppTheme {
+        ShowTwoLabels()
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HciAppTheme {
+        Greeting("Android")
+    }
+}
