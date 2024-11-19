585f528d4ca013538128f02be0e1a50c8ca29cc1
cachoJDG <jgago@itba.edu.ar>
2024-11-19 14:54:07 -0300

    add qr code icon and side bar

--- app/src/main/java/com/example/hciapp/components/BalloonNavigationBar.kt
+++ app/src/main/java/com/example/hciapp/components/BalloonNavigationBar.kt
@@ -1,9 +1,9 @@
 package com.example.hciapp.components
 
-
-
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.NavigationBar
@@ -11,13 +11,13 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hciapp.R
 
 @Composable
-fun BalloonNavigationBar(modifier: Modifier = Modifier)
-{
+fun BalloonNavigationBar(modifier: Modifier = Modifier) {
     NavigationBar(
         containerColor = colorScheme.primary,
         contentColor = colorScheme.onPrimary,
@@ -31,18 +31,36 @@ fun BalloonNavigationBar(modifier: Modifier = Modifier)
                 )
             },
             label = { Text(stringResource(R.string.home)) },
-
             selected = true,
-            onClick = {  }
-
+            onClick = { }
         )
-
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.qr_code),
+                    contentDescription = null
+                )
+            },
+            label = { Text(stringResource(R.string.qr)) },
+            selected = false,
+            onClick = { }
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = null
+                )
+            },
+            label = { Text(stringResource(R.string.profile)) },
+            selected = false,
+            onClick = { }
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun BalloonNavigationBarPreview()
-{
+fun BalloonNavigationBarPreview() {
     BalloonNavigationBar()
 }
